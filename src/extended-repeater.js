31442c73281c7d5dfef350babbd7f42a57577287
NastyJoey <59571074+NastyJoey@users.noreply.github.com>
2020-03-22 20:39:45 +0200

    feat: add repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,11 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let repeatTimes = options.repeatTimes,
+        separator = options.separator || '+',
+        addition = typeof options.addition === 'undefined' ? '' : String(options.addition),
+        additionRepeatTimes = options.additionRepeatTimes,
+        additionSeparator = options.additionSeparator || '|';
+
+
+    return (str + addition + (additionSeparator + addition).repeat(additionRepeatTimes - 1)) + (separator + str + addition + (additionSeparator + addition).repeat(additionRepeatTimes - 1)).repeat(repeatTimes - 1)
 };
-  
